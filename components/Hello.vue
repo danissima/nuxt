001285ae105a480f1c3316d794ec066ebd4ade05
Danila Odintsov <danila.odintsov@nutnet.ru>
2022-05-23 15:58:02 +0400

    initial

---
+++ components/Hello.vue
@@ -0,0 +1,19 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script lang="ts">
+interface IData {
+  message: string
+}
+
+export default {
+  name: 'NuxtTutorial',
+  data(): IData {
+    return {
+      message: 'Здарова )'
+    }
+  }
+}
+</script>
